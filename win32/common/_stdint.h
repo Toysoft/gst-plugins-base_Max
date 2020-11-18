1f687a8fab6952f395e63b322164e947d23ad403
Toysoft <toysofts@gmail.com>
2020-11-18 20:25:19 +0100

    First commit

---
+++ win32/common/_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _GST_PLUGINS_BASE__STDINT_H
+#define _GST_PLUGINS_BASE__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "gst-plugins-base 1.7.1"
+/* generated using gnu compiler gcc-5 (Debian 5.3.1-4) 5.3.1 20151219 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
